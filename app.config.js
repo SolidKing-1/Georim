9951487dcfab5b420e10a4f95882a413cca23ebe
Kingsley Oppong Appiah <kingsleyap3@gmail.com>
2025-09-14 13:54:54 -0500

    Before upgrade

--- app.config.js
+++ app.config.js
@@ -10,7 +10,7 @@ export default {
     android: {
       package: "com.yourcompany.georimapp", // <-- set this!
     },
-    plugins: ["expo-secure-store"],
+    plugins: ["expo-secure-store", "expo-web-browser"],
     extra: {
       GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
       BACKEND_URL: process.env.BACKEND_URL,
